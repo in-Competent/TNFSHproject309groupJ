8ababd164eea72c2f913a975c9816f5968e04708
Hermit <128883101+Hermit1003@users.noreply.github.com>
2023-04-10 14:33:05 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#導入Discord.py
+#這是拿來測試用的，當輸入“owo! repeat YourMessageHere"時機器人會輸出“YourMessageHere”，
 import discord
 #client 是我們與 Discord 連結的橋樑，intents 是我們要求的權限
 intents = discord.Intents.default()
@@ -18,12 +18,12 @@ async def on_message(message):
     if message.author == client.user:
         return
     #如果以「說」開頭
-    if message.content.startswith('說'):
+    if message.content.startswith('owo!'):
       #分割訊息成兩份
-      tmp = message.content.split(" ",2)
+      tmp = message.content.split("repeat ",2)
       #如果分割後串列長度只有1
       if len(tmp) == 1:
-        await message.channel.send("你要我說什麼啦？")
+        await message.channel.send("輸入完整語句")
       else:
         await message.channel.send(tmp[1])
 
